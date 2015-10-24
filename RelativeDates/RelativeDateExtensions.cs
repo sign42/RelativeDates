1e2cd26c4eb392150b26a928c52409b055996220
Andy Kirsch <andy.kirsch@gmail.com>
2015-10-24 15:44:51 -0400

    Added After

--- RelativeDates/RelativeDateExtensions.cs
+++ RelativeDates/RelativeDateExtensions.cs
@@ -11,13 +11,26 @@ namespace RelativeDates
 		/// Generates a random date before the this date, if one exists
 		/// </summary>
 		/// <param name="periodEnd">The date the random should be before</param>
-		/// <returns>A random date befor periodEnd</returns>
+		/// <returns>A random date before periodEnd</returns>
 		public static DateTime Before(this DateTime periodEnd) {
 			var rand = ThreadSafeRandom.NextDouble();
 			var start = DateTime.MinValue;
 			var range = periodEnd - start;
 			var adjustedTicksFromStart = (range.Ticks-1) * rand;
             return start + TimeSpan.FromTicks((long)adjustedTicksFromStart);
+		}
+
+		/// <summary>
+		/// Generates a random date after this date, if one exists
+		/// </summary>
+		/// <param name="periodStart">The date after which the result should be</param>
+		/// <returns>A random date after periodStart</returns>
+		public static DateTime After(this DateTime periodStart) {
+			var rand = ThreadSafeRandom.NextDouble();
+			var end = DateTime.MaxValue;
+			var range = end - periodStart;
+			var adjustedTicksFromStart = (range.Ticks + 1) * rand;
+			return periodStart + TimeSpan.FromTicks((long)adjustedTicksFromStart);
 		}
     }
 }
--- RelativeDatesTests/RelativeDateExtensionsTests.cs
+++ RelativeDatesTests/RelativeDateExtensionsTests.cs
@@ -14,7 +14,6 @@ namespace RelativeDatesTests {
 			Assert.AreEqual(baseDate, result);
 		}
 
-
 		[TestMethod]
 		public void BeforeNow() {
 			var results = new List<DateTime>();
@@ -28,7 +27,28 @@ namespace RelativeDatesTests {
 			AssertAllDifferent(results);
 		}
 
-		private void AssertAllDifferent(List<DateTime> values) {
+		[TestMethod]
+		public void AfterMaxDate() {
+			var baseDate = DateTime.MaxValue;
+			var result = baseDate.After();
+			// Going to accept this result
+			Assert.AreEqual(baseDate, result);
+		}
+
+		[TestMethod]
+		public void AfterNow() {
+			var results = new List<DateTime>();
+			var baseDate = DateTime.Now;
+			for (int i = 0; i < 100; i++) {
+				var result = baseDate.After();
+				Assert.IsTrue(result > baseDate);
+				results.Add(result);
+			}
+			// Check all of them against each other for uniqueness
+			AssertAllDifferent(results);
+		}
+
+		private static void AssertAllDifferent(List<DateTime> values) {
 			for(int i=0; i<values.Count; i++) {
 				for(int j=i+1; j<values.Count; j++) {
 					Assert.AreNotEqual(values[i], values[j]);
